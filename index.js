98b39d4dbf460f6615972a1be7cafe4a1b3d2504
justin <justinmwathi32@gmail.com>
2024-03-19 17:45:11 +0300

    Functions review lab

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if(dist<=400){
 }else if(dist>=400 && dist<2000){
     return price=(dist-400)*0.02
 }else if(dist>2000 && dist<=2500){
-    return(25);
+    return price=(25);
 }else{
     return "cannot travel that far"
 }  
